0446ef3b39831ef97063ff24b72ea3b3cb3d0688
Rafaela <rafi.lewis@me.com>
2019-02-19 20:16:12 -0500

    js pseudo code

---
+++ script.js
@@ -0,0 +1,12 @@
+// Step 1: Get user's data from a quiz (form). We'll filter through the object that Pet Finder returns to find the user's perfect match
+// 1) Location
+// 2) Drop down menu for age (ie. baby, young, adult, senior) that aligns with AGE filters on Pet Finder
+// 3) Size (ie. small, medium, large) that aligns with SIZE filters on Pet Finder
+// 4) Gender (ie. male, female, don't care) that aligns with GENDER filters on Pet Finder
+// 5) Hypoallergenic (yes, no, don't care). We'll filter through the breeds.list that Pet Finder returns (terrier = hypoallergenic, all others = not)
+// 6) House trained (yes, no, don't care).
+// 7) Has shots (yes, no, don't care)
+// 8) Spayed/Neutered (yes, no, don't care.)
+// 9) Do you have a desired breed? Textfield for breed or button option for don't care
+
+// We'll display the results based on user selection.
