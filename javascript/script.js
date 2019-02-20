0e2fb1742e529af548959173ce5647765f55a687
Rafaela <rafi.lewis@me.com>
2019-02-20 18:22:20 -0500

    html complete-ish

--- javascript/script.js
+++ javascript/script.js
@@ -9,7 +9,7 @@
 // 2) Drop down menu for age (ie. baby, young, adult, senior) that aligns with AGE filters on Pet Finder
 // 3) Size (ie. small, medium, large) that aligns with SIZE filters on Pet Finder
 // 4) Gender (ie. male, female, don't care) that aligns with GENDER filters on Pet Finder
-// 5) Hypoallergenic (yes, no, don't care). We'll filter through the breeds.list that Pet Finder returns (terrier = hypoallergenic, all others = not)
+// 5) Hypoallergenic (yes, no, don't care). We'll filter through the breeds.list that Pet Finder returns (terrier = hypoallergenic, all others = not) -- WE'LL COME BACK TO THAT ...
 // 6) House trained (yes, no, don't care).
 // 7) Has shots (yes, no, don't care)
 // 8) Spayed/Neutered (yes, no, don't care.)
